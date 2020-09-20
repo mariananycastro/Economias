5dd6c0e914cd86bd09c172903295807202ae33af
Mariana Yamada <mariana.yamada@rebase.com.br>
2020-09-20 16:24:01 -0300

    Add column name to category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_202414) do
+ActiveRecord::Schema.define(version: 2020_09_20_164650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_202414) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   add_foreign_key "accounts", "account_types"
