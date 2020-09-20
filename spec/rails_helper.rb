076101c39a3a4ce579ae7cde6ae65df7a6f1362c
Mariana Yamada <mariana.yamada@rebase.com.br>
2020-09-20 19:18:54 -0300

    Gitignor and rspec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,11 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'simplecov'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+SimpleCov.start
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -11,6 +16,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
