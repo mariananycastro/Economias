b6fd64a2b4ca7417d2bbb6fbbbae56b7a9186815
Mariana Yamada <mariana.yamada@rebase.com.br>
2020-09-27 17:34:44 -0300

    add edit update and destroy to account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -16,10 +16,31 @@ class AccountsController < ApplicationController
     render :new
   end
 
+  def edit
+    @account = account
+    @account_types = AccountType.all
+    @accounts = accounts
+  end
+
+  def update
+    @account = account
+
+    return redirect_to root_path if @account.update(params_account)
+
+    render :edit
+  end
+
+  def destroy
+    @account = account
+    return redirect_to root_path if @account.destroy
+
+    render :new
+  end
+
   private
 
   def accounts
-    Account.all
+    AccountDecorator.new(Account.all.order(:name)).decorate
   end
 
   def params_account
@@ -29,5 +50,9 @@ class AccountsController < ApplicationController
               :initial_value, 
               :account_type_id,
               :expiration_type)
+  end
+
+  def account
+    Account.find(params[:id])
   end
 end
