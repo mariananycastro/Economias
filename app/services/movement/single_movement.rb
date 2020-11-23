a95fb084693c4daf6437daff844ff7a6d491d1f6
Mariana Yamada <mariana.yamada@rebase.com.br>
2020-11-23 20:21:12 -0300

    Create class single movement

--- app/services/movement/single_movement.rb
+++ app/services/movement/single_movement.rb
@@ -16,18 +16,24 @@ class Movement::SingleMovement
 
   def update(id, params_movement)
     movement = Movement.find(id)
+    Movement::SingleMovement.altered(movement.installment_movement) if movement.installment_movement
     movement.update(params_movement)
   end
 
-  def self.delete(id, transfer)
-    new.delete(id, transfer)
+  def self.delete(id)
+    new.delete(id)
   end
 
-  def delete(id, transfer)
-    if transfer.empty?
-      Movement.destroy(id)
-    else
+  def delete(movement_id)
+    transfer = Transfer.movement_transfer(movement_id)
+    installment = InstallmentMovement.installment_of_movement(movement_id)
+
+    if !transfer.empty?
       Movement::Transfer.delete(transfer)
+    elsif installment
+      Movement::Installment.delete(installment)
+    else
+      Movement.destroy(movement_id)
     end
 
   end
