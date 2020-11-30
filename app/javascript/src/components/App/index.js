418845c1a6796806c0b42760004e8e707ac9a1e4
Mariana Yamada <mariana.yamada@rebase.com.br>
2020-11-30 11:12:12 -0300

    WIP studing react

--- app/javascript/src/components/App/index.js
+++ app/javascript/src/components/App/index.js
@@ -3,11 +3,9 @@ import React from 'react'
 class App extends React.Component {
   render () {
     return (
-      <div> Hello from App </div>
+      <div> Hello from App 2</div>
     )
   }
 }
 
 export default App
-
-
